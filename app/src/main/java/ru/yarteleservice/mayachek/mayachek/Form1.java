b6c967f052b2fdc7de2b010b7f81674f51ffdf46
donpadlo <donpadlo@gmail.com>
2017-06-23 17:48:24 +0300

    - пробую локализацию, дизайнерю

--- app/src/main/java/ru/yarteleservice/mayachek/mayachek/Form1.java
+++ app/src/main/java/ru/yarteleservice/mayachek/mayachek/Form1.java
@@ -1,15 +1,45 @@
 package ru.yarteleservice.mayachek.mayachek;
 
+import android.Manifest;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class Form1 extends AppCompatActivity {
+    String userid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form1);
+
+        // спрашиваем про права на доступ к координатам
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED) {
+
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION },
+                    12313);
+        };
+        ////////////
+        SharedPreferences mSettings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        userid=mSettings.getString("userid", "");
+        if (userid==""){
+          userid="-"+getString(R.string.you_id_real)+"-";
+        };
+
+        TextView vuserid = (TextView) findViewById(R.id.textView);
+        vuserid.setText(userid);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
