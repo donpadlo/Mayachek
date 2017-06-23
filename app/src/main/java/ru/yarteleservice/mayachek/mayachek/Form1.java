b14440cdb4cd83deeca5ce3e98051c91e62aab4c
donpadlo <donpadlo@gmail.com>
2017-06-23 14:45:27 +0300

    - прилепляю меню

--- app/src/main/java/ru/yarteleservice/mayachek/mayachek/Form1.java
+++ app/src/main/java/ru/yarteleservice/mayachek/mayachek/Form1.java
@@ -2,6 +2,7 @@ package ru.yarteleservice.mayachek.mayachek;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class Form1 extends AppCompatActivity {
 
@@ -10,4 +11,11 @@ public class Form1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form1);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
 }
