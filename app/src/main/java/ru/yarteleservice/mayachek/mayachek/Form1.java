8b569a278be538322faec2d8f087673a3e18b830
donpadlo <donpadlo@gmail.com>
2017-06-26 16:57:30 +0300

    - получаем новый userid

--- app/src/main/java/ru/yarteleservice/mayachek/mayachek/Form1.java
+++ app/src/main/java/ru/yarteleservice/mayachek/mayachek/Form1.java
@@ -3,13 +3,22 @@ package ru.yarteleservice.mayachek.mayachek;
 import android.Manifest;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Form1 extends AppCompatActivity {
     String userid;
@@ -24,28 +33,83 @@ public class Form1 extends AppCompatActivity {
                 PackageManager.PERMISSION_GRANTED &&
                 ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) ==
                         PackageManager.PERMISSION_GRANTED) {
+            Log.i("Info", "С правами на доступ к GPS всё хороршо");
 
         } else {
+            Log.i("Info", "Вывожу запрос на доступ к GPS");
             ActivityCompat.requestPermissions(this, new String[] {
                             Manifest.permission.ACCESS_FINE_LOCATION,
                             Manifest.permission.ACCESS_COARSE_LOCATION },
                     12313);
         };
         ////////////
+        Log.i("Info", "Читаю userid из сохраненных настроек");
         SharedPreferences mSettings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         userid=mSettings.getString("userid", "");
+
         if (userid==""){
-          userid="-"+getString(R.string.you_id_real)+"-";
+            Log.i("Info", "userid не найдет. Вероятно первый запуск. Пробуем получить!");
+            new GetNewUserid().execute();
+        } else {
+            Log.i("Info", "userid:"+userid);
+            TextView vuserid = (TextView) findViewById(R.id.textView);
+            vuserid.setText(userid);
+            //запускаем сервис ежели не запущен
         };
 
-        TextView vuserid = (TextView) findViewById(R.id.textView);
-        vuserid.setText(userid);
+
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    class GetNewUserid extends AsyncTask<String, Integer, String> {
+        @Override
+        protected String doInBackground(String... arg) {
+            String res;
+            BufferedReader reader = null;
+            res=null;
+            HttpURLConnection urlConnection = null;
+            Log.i("Info", "Запустил http соединение для получения нового ID");
+            try {
+                URL url = new URL("http://noc.yarteleservice.ru/getjsonmess.php?command=getnotreadmessages&username="+arg[0]+"&pass="+arg[1]+"&GLongitude="+arg[2]+"&GLatitude="+arg[3]+"&NLongitude="+arg[4]+"&NLatitude="+arg[5]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+                res = buffer.toString();
+                Log.i("Info", "Прочитали:"+res);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                Log.i("Info", "Соедениться не удалось..");
+                res="-ERROR: Ошибка соединения!";
+            }
+            return res;
+        };
+        @Override
+        protected void onPostExecute(String s) {
+            if (s.contains("-ERROR")!=true) {
+
+            } else {
+                Log.i("Info", "Показываю уведомление об ошибке");
+                Toast toast = Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT);
+                toast.show();
+            };
+
+        };
     }
 
 }
