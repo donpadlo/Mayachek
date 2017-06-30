c98b9ceae982cda5047628060ede6afcbe25f0bc
donpadlo <donpadlo@gmail.com>
2017-06-30 09:18:30 +0300

    - собран основной костяк

--- webgui/config.php
+++ webgui/config.php
@@ -1,8 +1,14 @@
 <?php
     $cfg=new Config();
     $cfg->sql_host="localhost";
-    $cfg->sql_base="maychek";
+    $cfg->sql_base="mayachek";
     $cfg->sql_user="mayachek";
     $cfg->sql_pass="PpPazx";
-    $cfg->connectDB();
+    $cfg->debug=true;
+    $cfg->connectDB();        
+    
+if ($cfg->debug) {
+	ini_set('display_errors', 1);
+	error_reporting(E_ALL);
+};
 ?>
--- webgui/index.php
+++ webgui/index.php
@@ -10,16 +10,24 @@
     $route=GetDef("route");
     $action=GetDef("action");
     
-    if ($route!=""):
-	if (is_file(WUO_ROOT."/client/server/".$route.".php")):
-	    include_once WUO_ROOT."/client/server/".$route.".php";    
+    if ($route!=""):	
+	$route=trim($route);		
+	if (is_file(WUO_ROOT."/controller/server/".$route.".php")):
+	    include_once WUO_ROOT."/controller/server/".$route.".php";    
 	else:
+	    if ($cfg->debug):
+		echo WUO_ROOT."/controller/server/".$route.".php\n";
+	    endif;
 	    die("Указаный файл не существует!");
 	endif;	
     endif;
     
     if ($action!=""):
-	include_once WUO_ROOT."/client/client/index.php";    
+	if (is_file(WUO_ROOT."/controller/client/".$action.".php")):
+	    include_once WUO_ROOT."/controller/client/index.php";    
+	else:
+	    die("Указаный файл не существует!");
+	endif;
     endif;
     
 
